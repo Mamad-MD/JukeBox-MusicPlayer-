33982e6ccdf2b5344a1f28e42821cf818347830a
Navid <navid5430@gmail.com>
2025-06-15 15:36:00 +0330

    host & join windows witout logic

---
+++ hostwindow.cpp
@@ -0,0 +1,24 @@
+#include "hostwindow.h"
+#include "mainwindow.h"
+#include "ui_hostwindow.h"
+
+HostWindow::HostWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::HostWindow)
+{
+    ui->setupUi(this);
+}
+
+HostWindow::~HostWindow()
+{
+    delete ui;
+}
+
+void HostWindow::on_PushButton_Back_clicked()
+{
+    auto mainWindow = new MainWindow;
+    mainWindow->show();
+    this->close();
+}
+}
+
---
+++ mainwindow.cpp
@@ -0,0 +1,32 @@
+#include "mainwindow.h"
+#include "hostwindow.h"
+#include "joinwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_PushButton_Host_clicked()
+{
+    auto hostWindow = new HostWindow;
+    hostWindow->show();
+    this->close();
+}
+
+
+void MainWindow::on_PushButton_Join_clicked()
+{
+    auto joinWindow = new JoinWindow;
+    joinWindow->show();
+    this->close();
+}
+
