b5cbbf4c843f73cd208ca11d7c150e78be42ce46
Robert <mohammad.msyb@gmial.com>
2025-06-30 18:16:00 +0330

    add login page

--- loginwindow.cpp
+++ loginwindow.cpp
@@ -1,33 +1,27 @@
 #include "loginwindow.h"
 #include "ui_loginwindow.h"
-#include <QMessageBox>
-#include <QPalette>
-#include <QBrush>
-#include <QPixmap>
 #include "authmanager.h"
+#include <QMessageBox>
+#include <QPixmap>
 
 
-LoginWindow::LoginWindow(Authmanager *authManager, QWidget *parent) :
-    QMainWindow(parent),
+LoginWindow::LoginWindow(Authmanager *authManager, QWidget *parent)
+    : QMainWindow(parent),
     ui(new Ui::LoginWindow),
     auth(authManager)
 {
     ui->setupUi(this);
 
-    // تنظیم سیگنال دکمه‌ها
+    QPixmap bg(":/images/login.jpg");
+    ui->backgroundLabel->setPixmap(bg.scaled(800, 600, Qt::IgnoreAspectRatio, Qt::SmoothTransformation));
+
+    this->setFixedSize(800, 600);
+    ui->loginPanel->setStyleSheet("background-color: rgba(255, 255, 255, 180); border-radius: 10px;");
+
     connect(ui->loginButton, &QPushButton::clicked, this, &LoginWindow::handleLogin);
     connect(ui->goToSignupButton, &QPushButton::clicked, this, &LoginWindow::goToSignup);
     connect(ui->forgotPasswordButton, &QPushButton::clicked, this, &LoginWindow::goToForgotPassword);
-
-    // تنظیم پس زمینه تصویر (کد جایگزین styleSheet برای اطمینان)
-    QPalette pal = this->palette();
-    pal.setBrush(QPalette::Window, QBrush(QPixmap(":/images/login.jpg").scaled(this->size(), Qt::KeepAspectRatioByExpanding)));
-    this->setPalette(pal);
-    this->setAutoFillBackground(true);
-
-    // تنظیم پنل شفاف (loginPanel باید در ui داشته باشی)
-    ui->loginPanel->setStyleSheet("background-color: rgba(255, 255, 255, 180); border-radius: 10px;");
-}
+   }
 
 LoginWindow::~LoginWindow()
 {
@@ -41,6 +35,7 @@ void LoginWindow::handleLogin()
 
     if (auth->validateLogin(username, password)) {
         QMessageBox::information(this, "Login", "Login successful!");
+        emit loginSucceeded(username);
     } else {
         QMessageBox::warning(this, "Login", "Invalid username or password.");
     }
