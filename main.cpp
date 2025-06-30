adc89f0b90921ed8281622c20903e20e9d1642aa
Robert <mohammad.msyb@gmial.com>
2025-07-01 01:48:07 +0330

    check all file

--- main.cpp
+++ main.cpp
@@ -16,36 +16,30 @@ int main(int argc, char *argv[])
     ForgotPasswordWindow forgotPassWin(&auth);
     MainWindow mainWin;
 
-    // لاگین موفق => نمایش پنجره اصلی، مخفی کردن لاگین
     QObject::connect(&loginWin, &LoginWindow::loginSucceeded, [&](const QString &) {
         mainWin.show();
         loginWin.hide();
     });
 
-    // بازگشت به لاگین پس از بستن پنجره اصلی (دلخواه)
     QObject::connect(&mainWin, &MainWindow::destroyed, [&]() {
         loginWin.show();
     });
 
-    // باز کردن صفحه ثبت نام
     QObject::connect(&loginWin, &LoginWindow::goToSignup, [&]() {
         signupWin.show();
         loginWin.hide();
     });
 
-    // بازگشت از ثبت نام به لاگین بعد از موفقیت ثبت نام
     QObject::connect(&signupWin, &SignUpWindow::registrationSucceeded, [&]() {
         signupWin.hide();
         loginWin.show();
     });
 
-    // باز کردن صفحه فراموشی رمز
     QObject::connect(&loginWin, &LoginWindow::goToForgotPassword, [&]() {
         forgotPassWin.show();
         loginWin.hide();
     });
 
-    // بعد از بستن فراموشی رمز، بازگشت به صفحه لاگین
     QObject::connect(&forgotPassWin, &ForgotPasswordWindow::destroyed, [&]() {
         loginWin.show();
     });
