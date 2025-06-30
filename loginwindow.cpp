aa8d24ff4aebaefd9384d0a190eb8bf3fee767b1
Robert <mohammad.msyb@gmial.com>
2025-06-30 23:26:01 +0330

    end of login page

--- loginwindow.cpp
+++ loginwindow.cpp
@@ -1,9 +1,7 @@
 #include "loginwindow.h"
 #include "ui_loginwindow.h"
 #include "authmanager.h"
-#include <QMessageBox>
-#include <QPixmap>
-
+#include "message_displayer.h"
 
 LoginWindow::LoginWindow(Authmanager *authManager, QWidget *parent)
     : QMainWindow(parent),
@@ -13,15 +11,15 @@ LoginWindow::LoginWindow(Authmanager *authManager, QWidget *parent)
     ui->setupUi(this);
 
     QPixmap bg(":/images/login.jpg");
-    ui->backgroundLabel->setPixmap(bg.scaled(800, 600, Qt::IgnoreAspectRatio, Qt::SmoothTransformation));
+    ui->backgroundLabel->setPixmap(bg.scaled(800, 450, Qt::IgnoreAspectRatio, Qt::SmoothTransformation));
 
-    this->setFixedSize(800, 600);
+    this->setFixedSize(800, 450);
     ui->loginPanel->setStyleSheet("background-color: rgba(255, 255, 255, 180); border-radius: 10px;");
 
     connect(ui->loginButton, &QPushButton::clicked, this, &LoginWindow::handleLogin);
     connect(ui->goToSignupButton, &QPushButton::clicked, this, &LoginWindow::goToSignup);
     connect(ui->forgotPasswordButton, &QPushButton::clicked, this, &LoginWindow::goToForgotPassword);
-   }
+}
 
 LoginWindow::~LoginWindow()
 {
@@ -34,9 +32,9 @@ void LoginWindow::handleLogin()
     QString password = ui->passwordEdit->text();
 
     if (auth->validateLogin(username, password)) {
-        QMessageBox::information(this, "Login", "Login successful!");
+        MessageDisplayer::display(MessageType::Info, "Login", "Login successful!");
         emit loginSucceeded(username);
     } else {
-        QMessageBox::warning(this, "Login", "Invalid username or password.");
+        MessageDisplayer::display(MessageType::Warning, "Login", "Invalid username or password.");
     }
 }
